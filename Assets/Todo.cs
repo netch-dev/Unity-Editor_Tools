3f779f41006e4e65374b6ae5168978a91a0b5c69
netch-dev <daijeln361@gmail.com>
2024-07-05 07:54:20 -0700

    Project planning

--- Assets/Todo.cs
+++ Assets/Todo.cs
@@ -1,20 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Todo : MonoBehaviour {
+public class Todo {
 	/* Brainstorm
      * 
      * 1. Change the colour of a folder
      * -  Right click on a folder and see a list of colours
+     * -- Using MenuItems, an editor class that allows you to display menus
      * 
      * 2. Change the icon of a folder
      * - Right click on the folder and see "Custom Icon..." option
      * 
      * 3. Reset the icon of a folder
+     * -- Using MenuItems
+     * --- Make sure the user is only able to select folders
      * 
      * 4. Save Data
      * - Using two techniques: EditorPrefs and custom json file
      * -- Save data using GUIDs (Global Unique Identifiers)
+     * --- Every folder and icon has a GUID
+     * ---- Data structure: Dictionary<string, string> where the key is the GUID of the folder and the value is the GUID of the icon
      */
 }
