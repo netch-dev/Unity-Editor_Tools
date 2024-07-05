3ae61b152ecd02692f4f9eec12d8879438cefd8c
netch-dev <daijeln361@gmail.com>
2024-07-05 07:50:17 -0700

    Initial commit

---
+++ Assets/Todo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Todo : MonoBehaviour {
+	/* Brainstorm
+     * 
+     * 1. Change the colour of a folder
+     * -  Right click on a folder and see a list of colours
+     * 
+     * 2. Change the icon of a folder
+     * - Right click on the folder and see "Custom Icon..." option
+     * 
+     * 3. Reset the icon of a folder
+     * 
+     * 4. Save Data
+     * - Using two techniques: EditorPrefs and custom json file
+     * -- Save data using GUIDs (Global Unique Identifiers)
+     */
+}
