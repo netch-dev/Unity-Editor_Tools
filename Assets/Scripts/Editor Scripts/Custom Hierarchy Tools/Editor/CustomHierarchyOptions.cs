bc1a613de1154a1b01c124b168d6976a743a6cae
netch-dev <daijeln361@gmail.com>
2024-07-07 13:50:54 -0700

    Todo 5.1 - Added a checkbox to each item in the hierarchy that toggles a gameobject

--- Assets/Scripts/Editor Scripts/Custom Hierarchy Tools/Editor/CustomHierarchyOptions.cs
+++ Assets/Scripts/Editor Scripts/Custom Hierarchy Tools/Editor/CustomHierarchyOptions.cs
@@ -11,7 +11,23 @@ namespace Netch.UtilityScripts {
 		}
 
 		private static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect) {
+			DrawActiveToggleButton(instanceID, selectionRect);
+		}
 
+		private static Rect DrawRect(float x, float y, float size) {
+			return new Rect(x, y, size, size);
+		}
+
+		private static void DrawActiveToggleButton(int id, Rect selectionRect) {
+			DrawButtonWithToggle(id, selectionRect.x - 20, selectionRect.y + 3, 10);
+		}
+
+		private static void DrawButtonWithToggle(int id, float x, float y, float size) {
+			GameObject gameObject = EditorUtility.InstanceIDToObject(id) as GameObject;
+			if (gameObject == null) return;
+
+			Rect rect = DrawRect(x, y, size);
+			gameObject.SetActive(GUI.Toggle(rect, gameObject.activeSelf, string.Empty));
 		}
 	}
 }
