a885fec58f21e34d883d1ab3d9e683f319bee66c
netch-dev <daijeln361@gmail.com>
2024-07-07 12:10:02 -0700

    UIBuilder_Example - Updated the design by changing the C# code

--- Assets/Scripts/Editor/UIBuilder_Example.cs
+++ Assets/Scripts/Editor/UIBuilder_Example.cs
@@ -16,9 +16,12 @@ public class UIBuilder_Example : EditorWindow {
 		// Each editor window contains a root VisualElement object
 		VisualElement root = rootVisualElement;
 
-		/*        // VisualElements objects can contain other VisualElement following a tree hierarchy.
-				VisualElement label = new Label("Hello World! From C#");
-				root.Add(label);*/
+		Label label = new Label("Example text...");
+		root.Add(label);
+
+		Button button = new Button();
+		button.text = "Click me!";
+		root.Add(button);
 
 		// Instantiate UXML
 		VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
