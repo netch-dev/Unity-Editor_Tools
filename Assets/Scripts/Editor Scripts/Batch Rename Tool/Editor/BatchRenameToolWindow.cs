3a84a8909e8cf51821097c628a9601f021ec4424
netch-dev <daijeln361@gmail.com>
2024-07-07 20:49:35 -0700

    BatchRenameToolWindow refactor

--- Assets/Scripts/Editor Scripts/Batch Rename Tool/Editor/BatchRenameToolWindow.cs
+++ Assets/Scripts/Editor Scripts/Batch Rename Tool/Editor/BatchRenameToolWindow.cs
@@ -46,7 +46,25 @@ namespace Netch.UtilityScripts {
 
 			EditorGUILayout.Space();
 			EditorGUILayout.LabelField("Step 3: Click the rename button", EditorStyles.boldLabel);
+
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.Space();
+			if (GUILayout.Button("Rename")) { // Create a button. the if statement checks when the button was pressed
+				RenameSelectedGameObjects();
+			}
+
+			EditorGUILayout.Space();
+			EditorGUILayout.EndHorizontal();
+
 			Repaint();
+		}
+
+		private void RenameSelectedGameObjects() {
+			int numberAsInt = int.Parse(batchNumber);
+			foreach (GameObject obj in Selection.objects) {
+				obj.name = $"{batchName}_{numberAsInt}";
+				numberAsInt++;
+			}
 		}
 	}
 }
