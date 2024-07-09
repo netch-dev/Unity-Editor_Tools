e5669644340c9012b561629b97a0b9a416959169
netch-dev <daijeln361@gmail.com>
2024-07-09 00:54:14 -0700

    * 10. Auto Save Tool - Added menu options to choose the save interval and auto saving functionality

--- Assets/Scripts/Editor Scripts/Auto Save Tool/Editor/AutoSaveEditor.cs
+++ Assets/Scripts/Editor Scripts/Auto Save Tool/Editor/AutoSaveEditor.cs
@@ -1,9 +1,21 @@
+using PlasticGui.WorkspaceWindow.Locks;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class AutoSaveEditor : EditorWindow {
 	private const string menuOption = "File/Autosave";
 	private static EditorWindow window;
+
+	private int selectedChoiceIndex;
+	private const string ONE_SECOND = "1 sec";
+	private const string THIRTY_SECONDS = "30 sec";
+	private const string ONE_MINUTE = "1 min";
+	private const string FIVE_MINUTES = "5 min";
+	private string[] choices = { ONE_SECOND, THIRTY_SECONDS, ONE_MINUTE, FIVE_MINUTES };
+	private float saveTime = 1;
+	private float nextSave = 0;
+
 	public static bool IsEnabled {
 		get => EditorPrefs.GetBool(menuOption, false);
 		set => EditorPrefs.SetBool(menuOption, value);
@@ -36,5 +48,46 @@ public class AutoSaveEditor : EditorWindow {
 		if (window != null) {
 			window.Close();
 		}
+	}
+
+	private void OnGUI() {
+		EditorGUILayout.LabelField("Interval: ");
+		EditorGUILayout.Space();
+
+		EditorGUI.BeginChangeCheck();
+		selectedChoiceIndex = EditorGUILayout.Popup("", selectedChoiceIndex, choices);
+		if (EditorGUI.EndChangeCheck()) {
+			switch (choices[selectedChoiceIndex]) {
+				case ONE_SECOND:
+					saveTime = 1;
+					break;
+
+				case THIRTY_SECONDS:
+					saveTime = 30;
+					break;
+
+				case ONE_MINUTE:
+					saveTime = 60;
+					break;
+
+				case FIVE_MINUTES:
+					saveTime = 300;
+					break;
+			}
+		}
+
+		if (IsEnabled) {
+			if (EditorApplication.timeSinceStartup > nextSave) {
+				string[] path = EditorSceneManager.GetActiveScene().path.Split('/');
+				bool saveSuccess = EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), string.Join("/", path));
+				nextSave = (float)EditorApplication.timeSinceStartup + saveTime;
+				if (saveSuccess) {
+					Debug.Log("Auto Save: Saved successfully");
+				} else {
+					Debug.LogError("Auto Save: Could not be saved");
+				}
+			}
+		}
+		Repaint();
 	}
 }
