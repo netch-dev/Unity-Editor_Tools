d2c043be8adb89152028ed8a15c0593bcbea00b1
netch-dev <daijeln361@gmail.com>
2024-07-09 00:40:31 -0700

    10. Auto Save Tool - Added a window that displays and hides when the autosave menu item is toggled

--- Assets/Scripts/Editor Scripts/Auto Save Tool/Editor/AutoSaveEditor.cs
+++ Assets/Scripts/Editor Scripts/Auto Save Tool/Editor/AutoSaveEditor.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class AutoSaveEditor : EditorWindow {
 	private const string menuOption = "File/Autosave";
+	private static EditorWindow window;
 	public static bool IsEnabled {
 		get => EditorPrefs.GetBool(menuOption, false);
 		set => EditorPrefs.SetBool(menuOption, value);
@@ -11,11 +12,29 @@ public class AutoSaveEditor : EditorWindow {
 	[MenuItem(menuOption, false, 175)]
 	public static void ToggleAutoSave() {
 		IsEnabled = !IsEnabled;
+		if (IsEnabled) {
+			ShowWindow();
+		} else {
+			CloseWindow();
+		}
 	}
 
 	[MenuItem(menuOption, true)]
 	private static bool ToggleAutoSaveValidate() {
 		Menu.SetChecked(menuOption, IsEnabled);
 		return true;
+	}
+
+	private static void ShowWindow() {
+		window = GetWindow(typeof(AutoSaveEditor));
+		GUIContent guiContent = new GUIContent("Auto Save Settings");
+		window.titleContent = guiContent;
+		window.Show();
+	}
+
+	private static void CloseWindow() {
+		if (window != null) {
+			window.Close();
+		}
 	}
 }
