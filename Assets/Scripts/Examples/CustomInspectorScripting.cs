39933c74bb8ab7bdfdb71bf74a37dc9b02099bfe
netch-dev <daijeln361@gmail.com>
2024-07-09 14:05:28 -0700

    Added a custom button to the inspector within the Transform component

---
+++ Assets/Scripts/Examples/CustomInspectorScripting.cs
@@ -0,0 +1,17 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Transform), true)]
+public class CustomInspectorScripting : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		// Adds a button to the inspector within the Transform component
+		if (GUILayout.Button("Click Me!")) {
+
+		}
+	}
+}
+*/
